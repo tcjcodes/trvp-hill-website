373d9174c2f9016dee417737fdd0b561764e9bdc
Trina Castillo <trina.castillo@expeditors.com>
2017-11-02 09:51:02 -0700

    dark typography theme with normalized styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-// import './index.css'
+import 'normalize.css';
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
       marginBottom: '1.45rem',
     }}
   >
@@ -19,11 +18,10 @@ const Header = () => (
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{margin: 0}}>
         <Link
           to="/"
           style={{
-            color: 'white',
             textDecoration: 'none',
           }}
         >
@@ -34,16 +32,16 @@ const Header = () => (
   </div>
 )
 
-const TemplateWrapper = ({ children }) => (
+const TemplateWrapper = ({children}) => (
   <div>
     <Helmet
       title="Gatsby Default Starter"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {name: 'description', content: 'Sample'},
+        {name: 'keywords', content: 'sample, something'},
       ]}
     />
-    <Header />
+    <Header/>
     <div
       style={{
         margin: '0 auto',
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,38 @@
 import Typography from "typography"
 import moragaTheme from "typography-theme-moraga"
 
+moragaTheme.bodyColor = 'hsl(0, 0%, 0%)';
+moragaTheme.headerColor = 'hsl(0, 100%, 100%)';
 moragaTheme.googleFonts.splice(0, 0, {
-    name: 'Lato',
-    styles: ['200', '400', '400i', '700'],
+  name: 'Lato',
+  styles: ['200', '400', '400i', '700'],
 })
 moragaTheme.headerFontFamily.splice(0, 0, 'Lato');
+moragaTheme.overrideThemeStyles = (data, options, styles) => {
+  return {
+    body: {
+      backgroundColor: '#000',
+      color: '#fff',
+    },
+    'a': {
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+      letterSpacing: data.rhythm(1/8),
+      color: 'rgb(158, 158, 158)'
+    },
+    'a:hover': {
+      color: 'rgb(230, 230, 230)',
+      textDecoration: 'none',
+      // fontWeight: 'bold'?
+    },
+    'small a': {
+      fontSize: data.rhythm(1/2)
+    },
+    'h1,h2,h3,h4,h5,h6': {
+      color: 'gainsboro',
+    },
+  };
+}
 
 const typography = new Typography(moragaTheme)
 
