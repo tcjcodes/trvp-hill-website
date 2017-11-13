926699f54490d2aa23a7dbf518c3b57af24c9233
Trina Castillo <trina.castillo@expeditors.com>
2017-11-13 11:33:16 -0800

    netlify form WIP & collapse contact inputs

--- src/components/contact-section.js
+++ src/components/contact-section.js
@@ -1,15 +1,71 @@
 import React from 'react'
 import { rhythm } from '../utils/typography';
+import { DefaultResponsive, Desktop } from '../utils/responsive'
 import { Section, SectionTitle } from './section'
-import { darkSecondaryColor } from '../utils/colors'
+import { darkSecondaryColor, lightColor, lightSecondaryColor } from '../utils/colors'
 import { primaryButton } from '../utils/buttons'
-import { Desktop } from '../utils/responsive'
-import { TextareaInputContainer, TextInputContainer } from './contact-inputs'
+import { secondaryFont } from '../utils/fonts'
+
+const labelCss = {
+    ...secondaryFont,
+    fontSize: rhythm(4 / 5),
+}
+const sharedInputCss = {
+    letterSpacing: '1px',
+    border: 0,
+    borderBottom: `1px solid ${lightSecondaryColor}`,
+    padding: `${rhythm(1 / 3)} 0`,
+    color: lightColor,
+    background: 'transparent',
+    fontSize: rhythm(6 / 10),
+    '&:focus': {
+        outline: 'none',
+        borderBottom: `2px solid ${lightColor}`,
+    },
+}
+const TextInput = (props) => (
+    <DefaultResponsive>{(isDesktop) =>
+        <div css={{
+            marginBottom: `${rhythm(1)}`,
+            width: isDesktop ? '28%' : '95%',
+            display: 'inline-block',
+            marginRight: isDesktop && rhythm(1)
+        }}>
+            <label css={labelCss} htmlFor={props.name}>{props.label}</label>
+            <br/>
+            <input
+                css={{
+                    ...sharedInputCss,
+                    width: '100%',
+                }}
+                name={props.name}
+                type={props.type || 'text'}
+                value={props.value}
+                onChange={props.onChange}/>
+        </div>
+    }</DefaultResponsive>
+)
+const TextareaInput = (props) => (
+    <div>
+        <label css={labelCss} htmlFor={props.name}>{props.label}</label>
+        <br/>
+        <textarea
+            css={{
+                ...sharedInputCss,
+                margin: `${rhythm(1 / 3)} 0 ${rhythm(1)} 0`,
+                resize: 'none',
+                height: rhythm(8),
+                width: '95%',
+            }}
+            name={props.name}
+            onChange={props.onChange}/>
+    </div>
+)
 
 const encode = (data) =>
     (Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&"))
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&'))
 
 class ContactContainer extends React.Component {
     constructor(props) {
@@ -17,14 +73,20 @@ class ContactContainer extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
     handleSubmit(e) {
-        !this.props.isMobile && alert('form submitted')
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", ...this.state })
+        alert('form submitted')
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({ 'form-name': 'contact', ...this.state })
         })
-            .then(() => alert("Success!"))
+            .then(() => {
+                alert('Success!');
+            })
             .catch(error => alert(error));
 
         e.preventDefault();
@@ -45,10 +107,11 @@ class ContactContainer extends React.Component {
                       data-netlify-honeypot='bot-field'
                       onSubmit={this.handleSubmit}
                       css={{ maxWidth: !this.props.isMobile && '900px', }}>
-                    <TextInputContainer name='name' label='Name'/>
-                    <TextInputContainer name='email' label='Email' type='email'/>
-                    <TextInputContainer name='subject' label='Subject'/>
-                    <TextareaInputContainer name='message' label='Message'/>
+                    <input hidden='true' name='bot-field' />
+                    <TextInput name='name' label='Name' onChange={this.handleChange} />
+                    <TextInput name='email' label='Email' type='email' onChange={this.handleChange} />
+                    <TextInput name='subject' label='Subject' onChange={this.handleChange} />
+                    <TextareaInput name='message' label='Message' onChange={this.handleChange} />
                     <button type='submit' css={{ ...primaryButton, marginTop: rhythm(1 / 2), }}>SUBMIT</button>
                 </form>
             </Section>
