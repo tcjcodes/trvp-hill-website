ab9c118f91d7d00d3b183ae79365690b773a5d7f
Cathryn <tcjcodes@users.noreply.github.com>
2018-03-01 15:54:16 -0800

    added google analytics (#7)

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # trvp-hill-website
-official website for music group Trap Hill
+Official website for music group Trap Hill
 
 favicon from [seattle by Fabien Jouin from the Noun Project](https://thenounproject.com/term/seattle/131555/)
 
-Using the [Gatsby CLI](https://www.gatsbyjs.org/docs/)
+Using the [Gatsby CLI](https://www.gatsbyjs.org/docs/), prefer npm (yarn issues)
 
 1. `gatsby develop` — Gatsby will start a hot-reloading development environment accessible at [localhost:8000](http://localhost:8000)
-1. Try editing the javascript pages in src/pages. Saved changes will live reload in the browser.
 1. `gatsby build` — Gatsby will perform an optimized production build for your site generating static HTML and per-route JavaScript code bundles.
 1. `gatsby serve` — Gatsby starts a local HTML server for testing your built site.
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
     pathPrefix: '/',
     siteMetadata: {
         spacedTitle: 'T R V P H I L L',
-        title: `TRVPHILL`,
+        title: `TRVPxHILL`,
         description: 'Official website of Trap Hill music',
         keywords: 'tacoma, olympia, washington, seattle, capitol hill, trvphill, rap, trap, hip hop, music, pacific northwest, PNW'
     },
@@ -12,6 +12,16 @@ module.exports = {
             resolve: `gatsby-plugin-typography`,
             options: {
                 pathToConfigModule: `src/utils/typography.js`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: `UA-115002255-1`,
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
             },
         },
         'gatsby-plugin-glamor',
